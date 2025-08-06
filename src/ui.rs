db90424a26912cf2db622d9f39964f05115feb93
eth0net <eth0net@pm.me>
2025-08-06 17:54:31 +0100

    extract ui panes module

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,8 @@
 mod components;
+mod panes;
 pub mod prelude;
 mod traits;
 
 pub use components::*;
+pub use panes::*;
 pub use prelude::*;
--- src/ui/components.rs
+++ src/ui/components.rs
@@ -2,7 +2,6 @@ pub mod button;
 pub mod icon;
 pub mod mod_details;
 pub mod mod_list;
-pub mod settings;
 pub mod text_input;
 pub mod tooltip;
 
@@ -10,6 +9,5 @@ pub use button::*;
 pub use icon::*;
 pub use mod_details::*;
 pub use mod_list::*;
-pub use settings::*;
 pub use text_input::*;
 pub use tooltip::*;
